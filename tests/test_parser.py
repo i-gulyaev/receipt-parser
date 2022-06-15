7603034d38e3e043f3adf9b48470c91e08e59b69
Igor Guliaev <igor.s.gulyaev@gmail.com>
2022-06-15 09:40:17 +0300

    Added unit tests for parser

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,6 @@
 import json
 import os
+from datetime import datetime
 from distutils import dir_util
 
 import pytest
@@ -19,6 +20,34 @@ def datadir(tmp_path, request):
 
 
 def test_parser_v1(datadir):
-    with open(datadir / "receipt_v1.json") as f:
-        result = parse_receipt(json.load(f))
+    with open(datadir / "receipt_v1.json") as a, open(
+        datadir / "receipt_v1_expected.json"
+    ) as e:
+        result = parse_receipt(json.load(a))
         assert len(result["items"]) == 1
+
+        expected = json.load(e)
+
+        for key, value in expected.items():
+            assert key in result
+            if type(result[key]) == datetime:
+                assert expected[key] == result[key].isoformat()
+            else:
+                assert expected[key] == result[key]
+
+
+def test_parser_v2(datadir):
+    with open(datadir / "receipt_v2.json") as a, open(
+        datadir / "receipt_v2_expected.json"
+    ) as e:
+        result = parse_receipt(json.load(a))
+        assert len(result["items"]) == 8
+
+        expected = json.load(e)
+
+        for key, value in expected.items():
+            assert key in result
+            if type(result[key]) == datetime:
+                assert expected[key] == result[key].isoformat()
+            else:
+                assert expected[key] == result[key]
